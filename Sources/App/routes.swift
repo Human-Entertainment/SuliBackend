d0ea562f2b0634b172b15dbd02e5487265c644a9
Bastian Inuk Christensen <bastian@inuk.blog>
2020-08-01 00:13:04 +0200

    fixes cors rules

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,6 +3,13 @@ import Vapor
 
 func routes(_ app: Application) throws
 {
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(cors)
     let registration = app.grouped("registration")
     try registration.register(collection: Registrations())
 }
